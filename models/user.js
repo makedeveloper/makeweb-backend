c92c3bca36d5ce3069d899503ab991aa6cc9f1c4
Shahryar-sss <saify.shahryar@gmail.com>
2020-07-01 09:42:02 +0530

    format schema

--- models/user.js
+++ models/user.js
@@ -1,32 +1,32 @@
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
     fullname: {
         type: String,
-        trim: true
+        trim: true,
     },
     username: {
         type: String,
         unique: true,
-        trim: true
+        trim: true,
     },
     email: {
         type: String,
         unique: true,
-        trim: true
+        trim: true,
     },
     password: {
         type: String,
-        required: true
-    }
+        required: true,
+    },
 });
 
 userSchema.methods.generateAuthToken = () => {
-    const token = jwt.sign({_id: this._id}, process.env.jwtprivateKey);
+    const token = jwt.sign({ _id: this._id }, process.env.jwtprivateKey);
     return token;
-}
+};
 
-const User = new mongoose.model('User', userSchema, "Users");
+const User = new mongoose.model("User", userSchema, "Users");
 
 module.exports.User = User;
